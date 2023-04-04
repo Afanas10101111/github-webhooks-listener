8514b5d1f21d75c07e35eeeba8819bdb67407b22
Afanas10101111 <68496260+Afanas10101111@users.noreply.github.com>
2023-04-04 22:48:48 +0300

    [GWL-0] Create sh script executor

---
+++ src/main/java/com/github/afanas10101111/gwl/exeption/ScriptFileAccessException.java
@@ -0,0 +1,7 @@
+package com.github.afanas10101111.gwl.exeption;
+
+public class ScriptFileAccessException extends RuntimeException {
+    public ScriptFileAccessException(String message) {
+        super(message);
+    }
+}
